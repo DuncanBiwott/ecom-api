6565832f3938dbe1349a761c4c09d50f6139dfd4
DuncanBiwott <bduncan19@student.kibu.ac.ke>
2023-03-09 20:49:58 +0300

    add spanID trace name

--- order-service/src/main/java/com/shoppingAPI/service/OrderService.java
+++ order-service/src/main/java/com/shoppingAPI/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.shoppingAPI.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shoppingAPI.dto.InventoryResponse;
 import com.shoppingAPI.dto.OrderLineItemsDto;
@@ -8,16 +7,18 @@ import com.shoppingAPI.dto.OrderRequest;
 import com.shoppingAPI.model.Order;
 import com.shoppingAPI.model.OrderLineItems;
 import com.shoppingAPI.repository.OrderRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
+
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +28,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
     private  final  ObjectMapper objectMapper;
     //private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
@@ -51,10 +53,12 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItems().stream()
                 .map(OrderLineItems::getSkuCode)
                 .toList();
+        Span inventorySpan=tracer.nextSpan().name("inventoryServiceCall");
 
-       // Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
-            // Call Inventory Service, and place order if product is in
-            // stock
+        try (Tracer.SpanInScope isLookUp = tracer.withSpan(inventorySpan.start())){
+
+            inventorySpan.tag("call", "inventory-service");
+
             InventoryResponse[] inventoryResponseArray = webClientBuilder.build().
                     get()
                     .uri("http://localhost:8085/api/inventory",
@@ -63,16 +67,25 @@ public class OrderService {
                     .bodyToMono(InventoryResponse[].class)
                     .block();
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+            boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
                     .allMatch(InventoryResponse::isInStock);
 
             if (allProductsInStock) {
                 orderRepository.save(order);
-               // kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                // kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
             }
+
+        } finally {
+            inventorySpan.end();
+        }
+
+       // Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+            // Call Inventory Service, and place order if product is in
+            // stock
+
 
     }
 
